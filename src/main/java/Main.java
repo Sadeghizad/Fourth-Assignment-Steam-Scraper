4099340d251d06306d85b8d443485f7dd7778034
Sadeghizad <mosadeghizad@gmail.com>
2025-04-23 14:15:49 +0330

    bunos sql done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,19 @@
 import java.io.IOException;
+import java.sql.*;
 import java.util.List;
 import java.util.Scanner;
 
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
 
         Parser parser = new Parser();
         parser.setUp("");
         Scanner input = new Scanner(System.in);
         System.out.println("Sort by: (1) Name (2) Rating (3) Price (4) Bonus wiki");
         int choice = input.nextInt();
-
+        boolean isGame = true;
         List<Game> result = switch (choice) {
             case 2 -> {
                 System.out.println("\n=== Highest rating ===");
@@ -23,7 +24,7 @@ public class Main {
                 yield parser.sortByPrice();
             }
             case 4 -> {
-
+                isGame = false;
                 System.out.println("\n=== wiki ===");
                 System.out.printf("%-45s | %8s | %s\n","NAME","YEAR","STUDIO");
                 Parser parser2 = new Parser();
@@ -40,6 +41,45 @@ public class Main {
             }
         };
         result.forEach(System.out::println);
+        if (isGame) {
+            try (Connection conn = DriverManager.getConnection("jdbc:sqlite:data.db")) {
+                String sql = "INSERT INTO Games (name, rating, price) VALUES (?, ?, ?)";
+                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                    for (Game game : result) {
+                        stmt.setString(1, game.getName());
+                        stmt.setDouble(2, game.getRating());
+                        stmt.setInt(3, game.getPrice());
+                        stmt.addBatch();
+                    }
+                    stmt.executeBatch();
+                } catch (SQLException e) {
+                    sql = """
+                              CREATE TABLE IF NOT EXISTS Games (
+                                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                  name TEXT,
+                                  rating REAL,
+                                  price INTEGER
+                              );
+                          """;
+
+                    try (Statement stmt = conn.createStatement()) {
+                        stmt.execute(sql);
+                        try (PreparedStatement stmt2 = conn.prepareStatement(sql)) {
+                            for (Game game : result) {
+                                stmt2.setString(1, game.getName());
+                                stmt2.setDouble(2, game.getRating());
+                                stmt2.setInt(3, game.getPrice());
+                                stmt2.addBatch();
+                            }
+                            stmt.executeBatch();
+                        } catch (SQLException er){
+                            er.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+        }
 
     }
 }
